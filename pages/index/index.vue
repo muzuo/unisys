85122ed0768acc16e8a8b6bfbc3cae8a04881c12
alading_muzuo <alading_muzuo@git.cloud.tencent.com>
2018-09-18 17:32:52 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="content">
+		<text class="title">{{title}}</text>
+		<text>{{rsg}}</text>
+		<button @click="clickBtn">点击按钮</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data: {
+			title: 'Hello'
+		},
+		beforeCreate:function(){
+			this.title='abc';
+			console.log('abbc');
+		},
+		onLoad:function(){
+			this.title='qqqqq';
+			console.log('qqqwe');
+		},
+		computed:{
+			rsg:function(){
+				return 1+1;
+			}
+		},
+		methods:{
+			clickBtn:function(){
+				uni.showToast({
+					title:'点击按钮',
+					icon:'success'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		flex: 1;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 36px;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,66 @@
+<template>
+	<form class='loginView' @submit="login">
+		<view class="oauth-image">
+			<image src="../../static/companyLogo.png" mode="aspectFit"></image>
+		</view>
+			<view class="label-3-text">易生支付手机报件</view>
+		<view class="input-view">
+			<view class="label-view">
+				<text class="label">账号 </text>
+			</view>
+			<input class="input" type="text" placeholder="请输入用户名" name="userName" />
+		</view>
+		<view class="input-view">
+			<view class="label-view">
+				<text class="label">密码</text>
+			</view>
+			<input class="input" :password="showPassword" placeholder="请输入密码" name="userWord" />
+			<view class="uni-icon uni-icon-eye" :class="[!showPassword ? 'uni-active' : '']" @click="changePassword"></view>
+		</view>
+		<view class="button-view">
+			<button type="default" class="login" hover-class="hover" formType="submit">登录</button>
+			<button type="default" class="register" hover-class="hover" formType="reset">重置</button>
+		</view>
+	</form>
+</template>
+
+<script>
+	export default {
+		data: {
+showPassword:true
+		},
+		methods: {
+			/*表单提交登录*/
+			login(e) {
+				console.log(e);
+				console.log(this.showPassword);
+			},
+			changePassword(){
+				this.showPassword=!this.showPassword;
+			}
+		}
+	}
+</script>
+
+
+<style>
+	.label-3-text {
+		color: #576B95;
+		font-size: 1em;
+		margin: 50px 0 50px 35%;
+	}
+	.oauth-image {
+        width: 9em;
+        height: 9em;
+        border: 1px  #dddddd;
+        border-radius: 100px;
+        margin: auto;
+        background-color: #ffffff;
+    }
+
+    .oauth-image image {
+        width: 7.5em;
+        height: 7.5em;
+        margin: 20px;
+    }
+</style>
